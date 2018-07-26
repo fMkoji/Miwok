671b042422c5151689a3f997a2c74322d8550aa7
Frank Mchana <frankmkoji@gmail.com>
2018-07-26 21:51:26 +0300

    Displayed the list on numbers activity using while and for loops.

--- app/src/main/java/com/example/android/miwok/NumbersActivity.java
+++ app/src/main/java/com/example/android/miwok/NumbersActivity.java
@@ -3,6 +3,9 @@ package com.example.android.miwok;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
+import android.widget.LinearLayout;
+import android.widget.RelativeLayout;
+import android.widget.TextView;
 
 import java.util.ArrayList;
 
@@ -13,31 +16,35 @@ public class NumbersActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_numbers2);
 
-        ArrayList <String> list = new ArrayList<String>();
-        list.add(0,"One");
-        list.add(1,"Two");
-        list.add(2,"Three");
-        list.add(3,"Four");
-        list.add(4,"Five");
-        list.add(5,"Six");
-        list.add(6,"Seven");
-        list.add(7,"Eight");
-        list.add(8,"Nine");
-        list.add(9,"Ten");
+        ArrayList<String> list = new ArrayList<String>();
+        list.add(0, "One");
+        list.add(1, "Two");
+        list.add(2, "Three");
+        list.add(3, "Four");
+        list.add(4, "Five");
+        list.add(5, "Six");
+        list.add(6, "Seven");
+        list.add(7, "Eight");
+        list.add(8, "Nine");
+        list.add(9, "Ten");
 
-//String[] words =new String[10];
-//words[0] = "One";
-//words[1] = "Two";
-//words[2] = "Three";
-//words[3] = "Four";
-//words[4] = "Five";
-Log.v("NumberActivity", "The word at index 4: " + list.get(4));
-//words[5] = "Six";
-//words[6] = "Seven";
-Log.v("NumberActivity", "The word at index 6: " + list.get(6));
-//words[7] = "Eight";
-//words[8] = "Nine";
-//words[9] = "Ten";
+        LinearLayout rootView = (LinearLayout) findViewById(R.id.rootView);
+
+
+//        int index = 0;
+//
+//        while (index <= 9) {
+//
+//            TextView wordView = new TextView(this);
+//            wordView.setText(list.get(index));
+//            rootView.addView(wordView);
+//            index++;
+//        }
+        for (int index = 0; index < 10; index++) {
+            TextView wordView = new TextView(this);
+            wordView.setText(list.get(index));
+            rootView.addView(wordView);
+        }
 
     }
 
